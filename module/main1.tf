86bdbb3238b90ec7f8b88d911c69b6df7e1c7562
paras-31 <paras.kamboj@cloudeq.com>
2023-01-14 01:09:05 +0530

    done

--- module/main1.tf
+++ module/main1.tf
@@ -3,7 +3,7 @@ module "ec2" {
   for_each = var.tags
   ami = each.value["ami"]
   instatype = each.value["instance_type"]
-  Name = each.value["name"]
+  name = each.value["name"]
   owner = each.value["owner"]
   purpose = each.value["purpose"]
 
